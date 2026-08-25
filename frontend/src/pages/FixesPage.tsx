41dd5b9070faa556420b45049eb51075426c3707
Adithya Wijaya <adithyawijayajr@gmail.com>
2026-08-25 18:00:21 +0800

    debug to release

--- frontend/src/pages/FixesPage.tsx
+++ frontend/src/pages/FixesPage.tsx
@@ -12,8 +12,8 @@ const fixes = [
     title: "DiTXFixer (Required)",
     description:
       "Fix that will solve the problem of games disappearing, changing to purchase, no internet connection. Extract and copy dwmapi.dll, xinput1_4.dll and OpenSteamTool.dll to your Steam root directory. If there is still no internet connection, try placing the manifest in C:\\Program Files (x86)\\Steam\\depotcache.",
-    url: "https://github.com/madoiscool/BetterSteamTools/releases/latest/download/OpenSteamTool-v1.0.0-Debug.zip",
-    download: "OpenSteamTool-v1.0.0-Debug.zip",
+    url: "https://github.com/madoiscool/BetterSteamTools/releases/latest/download/OpenSteamTool-v1.0.0-Release.zip",
+    download: "OpenSteamTool-v1.0.0-Release.zip",
   },
   {
     icon: "cloud",
